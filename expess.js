bd928025b01556d93a68bc7a2fb92dc5c2300ea3
agile8118 <agile.8272@gmail.com>
2026-01-23 09:44:58 -0500

    Cleans up error handling for Cpeak and Express

--- expess.js
+++ expess.js
@@ -61,7 +61,7 @@ app.patch(`/update-something/:id/:name`, (req, res) => {
 });
 
 // Inserts a simple record to the database
-app.post("/code", async (req, res, handleErr) => {
+app.post("/code", async (req, res) => {
   const code = generateCode();
 
   // Create a new code record
@@ -81,177 +81,155 @@ app.post("/code", async (req, res, handleErr) => {
       // Unique violation
       return res.status(409).json({ error: "Code already exists." });
     }
-    return handleErr(err);
+    throw err;
   }
 });
 
 // Reads a simple random code from the database and returns it
-app.get("/code-v1", async (req, res, handleErr) => {
-  try {
-    const result = await DB.query(
-      `
+app.get("/code-v1", async (req, res) => {
+  const result = await DB.query(
+    `
       SELECT id, code, created_at
       FROM codes
       ORDER BY RANDOM()
       LIMIT 1
     `,
-    );
+  );
 
-    if (result.length === 0) {
-      return res.status(404).json({ error: "No codes found." });
-    }
-
-    res.json({ data: result[0] });
-  } catch (err) {
-    return handleErr(err);
+  if (result.length === 0) {
+    return res.status(404).json({ error: "No codes found." });
   }
+
+  res.json({ data: result[0] });
 });
 
-app.get("/code-v2", async (req, res, handleErr) => {
-  try {
-    const countResult = await DB.query(`SELECT COUNT(*) FROM codes`);
-    const count = parseInt(countResult[0].count, 10);
+app.get("/code-v2", async (req, res) => {
+  const countResult = await DB.query(`SELECT COUNT(*) FROM codes`);
+  const count = parseInt(countResult[0].count, 10);
 
-    if (count === 0) return res.status(404).json({ error: "No codes found." });
+  if (count === 0) return res.status(404).json({ error: "No codes found." });
 
-    // Generate a random ID between 1 and Count
-    const randomId = crypto.randomInt(1, count + 1);
+  // Generate a random ID between 1 and Count
+  const randomId = crypto.randomInt(1, count + 1);
 
-    // Fetch the record by ID (Index Lookup)
-    const result = await DB.query(
-      `
+  // Fetch the record by ID (Index Lookup)
+  const result = await DB.query(
+    `
       SELECT id, code, created_at
       FROM codes
       WHERE id = $1
     `,
-      [randomId],
-    );
+    [randomId],
+  );
 
-    if (result.length === 0) {
-      return res.status(404).json({ error: "record not found" });
-    }
-
-    res.json({ data: result[0] });
-  } catch (err) {
-    return handleErr(err);
+  if (result.length === 0) {
+    return res.status(404).json({ error: "record not found" });
   }
+
+  res.json({ data: result[0] });
 });
 
-app.get("/code-v3", async (req, res, handleErr) => {
-  try {
-    const maxResult = await DB.query(
-      `SELECT id FROM codes ORDER BY id DESC LIMIT 1`,
-    );
+app.get("/code-v3", async (req, res) => {
+  const maxResult = await DB.query(
+    `SELECT id FROM codes ORDER BY id DESC LIMIT 1`,
+  );
 
-    if (maxResult.length === 0)
-      return res.status(404).json({ error: "No codes found." });
+  if (maxResult.length === 0)
+    return res.status(404).json({ error: "No codes found." });
 
-    const maxId = maxResult[0].id;
+  const maxId = maxResult[0].id;
 
-    // Generate random ID up to Max
-    const randomId = crypto.randomInt(1, maxId + 1);
+  // Generate random ID up to Max
+  const randomId = crypto.randomInt(1, maxId + 1);
 
-    // Fetch (Index Lookup)
-    const result = await DB.query(
-      `SELECT id, code, created_at FROM codes WHERE id = $1`,
-      [randomId],
-    );
+  // Fetch (Index Lookup)
+  const result = await DB.query(
+    `SELECT id, code, created_at FROM codes WHERE id = $1`,
+    [randomId],
+  );
 
-    if (result.length === 0) {
-      return res.status(404).json({ error: "record not found" });
-    }
-
-    res.json({ data: result[0] });
-  } catch (err) {
-    return handleErr(err);
+  if (result.length === 0) {
+    return res.status(404).json({ error: "record not found" });
   }
+
+  res.json({ data: result[0] });
 });
 
-app.get("/code-v4", async (req, res, handleErr) => {
-  try {
-    const randomId = crypto.randomInt(1, 700000 + 1);
+app.get("/code-v4", async (req, res) => {
+  const randomId = crypto.randomInt(1, 700000 + 1);
 
-    // Fetch the record by ID (Index Lookup)
-    const result = await DB.query(
-      `
+  // Fetch the record by ID (Index Lookup)
+  const result = await DB.query(
+    `
       SELECT id, code, created_at
       FROM codes
       WHERE id = $1
     `,
-      [randomId],
-    );
+    [randomId],
+  );
 
-    if (result.length === 0) {
-      return res.status(404).json({ error: "record not found" });
-    }
-
-    res.json({ data: result[0] });
-  } catch (err) {
-    return handleErr(err);
+  if (result.length === 0) {
+    return res.status(404).json({ error: "record not found" });
   }
+
+  res.json({ data: result[0] });
 });
 
 // Inserts a simple record to the database through Redis for super fast O(1) operations
-app.post("/code-fast", async (req, res, handleErr) => {
+app.post("/code-fast", async (req, res) => {
   const code = generateCode();
 
-  try {
-    // Check uniqueness (O(1))
-    // SADD returns 1 if added (new), 0 if exists (duplicate)
-    const isNew = await redis.sadd("codes:unique", code);
+  // Check uniqueness (O(1))
+  // SADD returns 1 if added (new), 0 if exists (duplicate)
+  const isNew = await redis.sadd("codes:unique", code);
 
-    if (isNew === 0) {
-      return res.status(409).json({ error: "Code already exists." });
-    }
-
-    // Generate ID (Incrementing Sequence O(1))
-    const id = await redis.incr("codes:seq");
-    const created_at = new Date().toISOString();
-
-    // Pipeline these for speed (1 network round trip instead of 2)
-    const pipeline = redis.pipeline();
-    // Store Data (O(1) Hash Set)
-    pipeline.hset(`code:${id}`, { id, code, created_at });
-
-    // We will add the ids to a queue so that later a background worker can sync to Postgres
-    pipeline.lpush("codes:sync_queue", id);
-
-    await pipeline.exec();
-
-    res.status(201).json({
-      created_code: { id, code, created_at },
-    });
-  } catch (err) {
-    return handleErr(err);
+  if (isNew === 0) {
+    return res.status(409).json({ error: "Code already exists." });
   }
+
+  // Generate ID (Incrementing Sequence O(1))
+  const id = await redis.incr("codes:seq");
+  const created_at = new Date().toISOString();
+
+  // Pipeline these for speed (1 network round trip instead of 2)
+  const pipeline = redis.pipeline();
+  // Store Data (O(1) Hash Set)
+  pipeline.hset(`code:${id}`, { id, code, created_at });
+
+  // We will add the ids to a queue so that later a background worker can sync to Postgres
+  pipeline.lpush("codes:sync_queue", id);
+
+  await pipeline.exec();
+
+  res.status(201).json({
+    created_code: { id, code, created_at },
+  });
 });
 
 // Gets a code but through Redis for super fast O(1) lookups
-app.get("/code-fast", async (req, res, handleErr) => {
-  try {
-    // Get max ID
-    const maxId = await getMaxId();
-    if (maxId === 0) return res.status(404).json({ error: "No codes found." });
+app.get("/code-fast", async (req, res) => {
+  // Get max ID
+  const maxId = await getMaxId();
+  if (maxId === 0) return res.status(404).json({ error: "No codes found." });
 
-    // Generating a random ID
-    const randomId = crypto.randomInt(1, maxId + 1);
+  // Generating a random ID
+  const randomId = crypto.randomInt(1, maxId + 1);
 
-    // Fetch the code (O(1) Hash Lookup)
-    const result = await redis.hgetall(`code:${randomId}`);
+  // Fetch the code (O(1) Hash Lookup)
+  const result = await redis.hgetall(`code:${randomId}`);
 
-    if (Object.keys(result).length === 0) {
-      return res.status(404).json({ error: "record not found" });
-    }
-
-    res.json({ data: result });
-  } catch (err) {
-    return handleErr(err);
+  if (Object.keys(result).length === 0) {
+    return res.status(404).json({ error: "record not found" });
   }
+
+  res.json({ data: result });
 });
 
-app.use((err, req, res, next) => {
-  console.error("Unhandled error:", err);
-  res.status(500).json({ error: "Internal Server Error" });
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).json({
+    error: "Sorry, something unexpected happened on our side.",
+  });
 });
 
 const PORT = 3001;
